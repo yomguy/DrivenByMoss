0f41dbbf972cff29e570f7f6c9c005aa3043a807
mos <mos@UbuntuStudioLenovo>
2022-11-20 16:09:29 +0100

    Update to latest JDK.

--- release-linux.sh
+++ release-linux.sh
@@ -1,2 +1,7 @@
-export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
+if [ -d "/home/mos/java/jdk-17.0.5+8" ]; then
+    export JAVA_HOME=/home/mos/java/jdk-17.0.5+8
+else
+    export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
+fi
+
 mvn clean install package -Dbitwig.extension.directory=target
