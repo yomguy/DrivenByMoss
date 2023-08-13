5dd330b5a41d94f3ec48ebc74261c6a221872f12
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2023-08-13 11:40:39 +0200

    use the java version from the user home by default or the debian system one (could be adapted to other distributions)

--- release-linux.sh
+++ release-linux.sh
@@ -1,5 +1,7 @@
 if [ -d "/home/mos/java/jdk-17.0.5+8" ]; then
     export JAVA_HOME=/home/mos/java/jdk-17.0.5+8
+elif [ -d "/home/$USER/java/jdk-17.0.5+8" ]; then
+    export JAVA_HOME=/home/$USER/java/jdk-17.0.5+8
 else
     export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
 fi
